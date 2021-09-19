f45fe81fd4fde36edf35750c4b13b8e60b18fd07
Rene Ott <rene.ott6@gmail.com>
2021-09-19 22:17:29 +0300

    Add repositories

--- run.cmd
+++ run.cmd
@@ -1,4 +1,5 @@
 @echo off
-SET antpath="apache-ant-1.10.5\bin\"
+SET antpath="buildconfig\apache-ant-1.10.5\bin\"
 
-%antpath%ant -f build.xml runclient
+call %antpath%ant -f buildconfig\build.xml runclient
+pause
