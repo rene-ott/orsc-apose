0c5b06b794827c74ec87c764267518230e9eeafe
rene-ott <60364564+rene-ott@users.noreply.github.com>
2021-09-28 22:09:28 +0300

    feat/add-script-search (#3)

--- src/com/aposbot/ScriptFrame.java
+++ src/com/aposbot/ScriptFrame.java
@@ -7,13 +7,15 @@ import com.aposbot._default.IScriptListener;
 import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
+import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class ScriptFrame extends Frame {
 
@@ -22,6 +24,7 @@ public final class ScriptFrame extends Frame {
     private static final String PROCESSING_ERROR = "Error processing script. Send this output to the script's author:";
     private final java.awt.List displayed_list;
     private final TextField field;
+    private TextField searchField;
     private final IClient client;
     private ScriptEngineManager manager;
 
@@ -50,34 +53,51 @@ public final class ScriptFrame extends Frame {
         scroll.add(displayed_list);
         add(scroll, BorderLayout.CENTER);
 
+        // Search panel
+        final Panel searchPanel = new Panel();
+        searchPanel.setLayout(new GridLayout(1, 0));
+        Label searchLabel = new Label("Search:");
+        searchLabel.setFont(Constants.UI_FONT);
+        searchPanel.add(searchLabel);
+
+        searchField = new TextField("");
+        searchField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                    displayed_list.select(getMatchedScriptIndex());
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+
+        searchPanel.add(searchField);
+        searchField.setFont(Constants.UI_FONT);
+
         final Panel buttonPanel = new Panel();
 
         final Button okButton = new Button("OK");
         okButton.setFont(Constants.UI_FONT);
-        okButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        initScript();
-                    }
-                }, "ScriptInit").start();
-            }
-        });
+        okButton.addActionListener(e -> new Thread(this::initScript, "ScriptInit").start());
         buttonPanel.add(okButton);
 
         final Button cancelButton = new Button("Cancel");
         cancelButton.setFont(Constants.UI_FONT);
-        cancelButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setVisible(false);
-            }
-        });
+        cancelButton.addActionListener(e -> setVisible(false));
         buttonPanel.add(cancelButton);
 
-        add(buttonPanel, BorderLayout.SOUTH);
+        final Panel southPanel = new Panel();
+        southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.Y_AXIS));
+        southPanel.add(searchPanel);
+        southPanel.add(buttonPanel);
+        add(southPanel, BorderLayout.SOUTH);
 
         pack();
         setMinimumSize(getSize());
@@ -85,6 +105,37 @@ public final class ScriptFrame extends Frame {
         final Insets in = getInsets();
 
         setSize(in.right + in.left + 310, in.top + in.bottom + 240);
+    }
+
+    private int getMatchedScriptIndex() {
+        String inputText = searchField.getText().toLowerCase();
+        String[] scripts = displayed_list.getItems();
+        if (scripts.length == 0)
+            return -1;
+
+        List<Object[]> matchedScripts = new ArrayList<>();
+
+        for (int i = 0; i < scripts.length; i ++) {
+            String curScriptName = scripts[i].toLowerCase();
+
+            if (curScriptName.equals(inputText))
+                return i;
+
+            if (curScriptName.contains(inputText)) {
+                matchedScripts.add(new Object[] { curScriptName, i});
+            }
+        }
+        if (matchedScripts.isEmpty())
+            return -1;
+
+        if (matchedScripts.size() == 1)
+            return (Integer) matchedScripts.get(0)[1];
+
+        List<Object[]> sortedMatchedScripts = matchedScripts.stream().sorted((o1, o2) -> {
+            return ((String)o1[0]).length() - ((String)o2[0]).length();
+        }).collect(Collectors.toList());
+
+        return (Integer)sortedMatchedScripts.get(0)[1];
     }
 
     public void initScript() {
@@ -207,7 +258,6 @@ public final class ScriptFrame extends Frame {
 
     public void process(String filename, List<String> list) {
         File bonusdir = new File((filename));
-        System.out.println(filename);
         filename = filename.replace(dir.toString() + File.separator, "");
         if (bonusdir.isDirectory()) {
             String[] bonusFiles = bonusdir.list();
