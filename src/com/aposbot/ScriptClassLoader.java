0a16ff640a93d91ae297077632f5868cfeeec47d
rene-ott <60364564+rene-ott@users.noreply.github.com>
2021-10-17 10:59:04 +0300

    feat/update-painted-info-and-load-scripts-from-subfolder (#6)
    * Make scripts which are any subdirectory of Scripts dir and contain nested classes executable
    
    * Format user info differently in PaintListener
    
    * Add bot

--- src/com/aposbot/ScriptClassLoader.java
+++ src/com/aposbot/ScriptClassLoader.java
@@ -5,13 +5,31 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public final class ScriptClassLoader extends ClassLoader {
 
     private final HashMap<String, Class<?>> classes = new HashMap<>();
+    private final HashMap<String, String> subDirectoryScriptsWithNestedClasses = new HashMap<>();
+    private static final Pattern pattern = Pattern.compile("(\\\\(?<path>[a-zA-Z]+)\\\\)?(?<className>[a-zA-Z_0-9]+)(?<innerClass>\\$[a-zA-Z_0-9]+)?");
 
     @Override
     public Class<?> loadClass(String name) throws ClassNotFoundException {
+        Matcher m = pattern.matcher(name);
+        if (m.matches()) {
+            String className = m.group("className");
+
+            String innerClass = m.group("innerClass");
+            if (innerClass == null) {
+                String path = m.group("path");
+                subDirectoryScriptsWithNestedClasses.put(className, path);
+            } else {
+                String path = subDirectoryScriptsWithNestedClasses.get(className);
+                name = "\\" + path + "\\" + name;
+            }
+        }
+        
         final File file = new File(ScriptFrame.dir, name + ".class");
         if (!file.exists()) {
             return super.loadClass(name);
