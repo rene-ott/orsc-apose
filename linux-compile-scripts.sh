6237aff396e12876301b3d26e5ca15f0aed77d52
Rene Ott <rene.ott6@gmail.com>
2022-03-20 02:28:51 +0200

    Copy misc script class files to Scripts dir after compilation

--- linux-compile-scripts.sh
+++ linux-compile-scripts.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 ./buildconfig/apache-ant-1.10.5/bin/ant -f buildconfig/build.xml compile-scripts
+if [[ -d ./Scripts/misc ]]
+then
+    echo "Moving class files from ./Scripts/misc directory ./Scripts directory"
+	mv ./Scripts/misc/*.class ./Scripts
+fi
 read -n 1 -s -r -p "Press any key to continue"
--- windows-compile-scripts.cmd
+++ windows-compile-scripts.cmd
@@ -2,4 +2,9 @@
 SET antpath="buildconfig\apache-ant-1.10.5\bin\"
 
 call %antpath%ant -f buildconfig\build.xml compile-scripts
+
+if exist Scripts\misc (
+  echo "Moving class files from ./Scripts/misc directory ./Scripts directory"
+  XCOPY /S Scripts\misc\*.class Scripts\*.class
+)
 pause
